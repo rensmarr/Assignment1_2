166339453ac6e4e4090fb8a1d64a71d7dd6fb4ce
Fatemeh Hashemi <62609785+Fhashemi91@users.noreply.github.com>
2021-10-06 20:41:02 +0200

    Training component

--- training/main.py
+++ training/main.py
@@ -1,6 +1,10 @@
+import re
+
 import pandas as pd
 from google.cloud import storage
-
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
 
 def fetch_train_data():
     bucket_name = "raw-titanic-data"
@@ -11,23 +15,49 @@ def fetch_train_data():
     return df
 
 
-def train_model_from_data(train_data):
-    from sklearn.ensemble import RandomForestClassifier
+def train_model_from_data(train_data_df):
+    train_data_df.drop("Cabin", axis=1, inplace=True)
 
-    y = train_data["Survived"]
+    train_data_df['Salutation'] = train_data_df['Name'].apply(split_it)
 
-    features = ["Pclass", "Sex", "SibSp", "Parch"]
-    X = pd.get_dummies(train_data[features])
-    X_test = pd.get_dummies(test_data[features])
+    train_data_df["Age"].fillna(
+        train_data_df.groupby("Salutation")["Age"].transform("median"),
+        inplace=True
+    )
 
-    model = RandomForestClassifier(n_estimators=100, max_depth=5, random_state=1)
-    model.fit(X, y)
-    predictions = model.predict(X_test)
-    
-    output = pd.DataFrame({'PassengerId': test_data.PassengerId, 'Survived': predictions})
-    output.to_csv('submission.csv', index=False)
-    print("Your submission was successfully saved!")
-    print(train_data_df)
+    train_data_df['Surname'] = train_data_df.Name.map(
+        lambda x: x.split(',')[0]
+    )
+
+    to_map = {'Icard': 'C', 'Stone': 'S'}
+
+    train_data_df.Embarked.fillna(
+        train_data_df.Surname.map(to_map), inplace=True
+    )
+
+    train_data_df['Embarked'].replace({'S': 1, 'C': 2, 'Q': 3}, inplace=True)
+
+    train_data_df['Sex'].replace({'male': 0, 'female': 1}, inplace=True)
+
+    train_data_df.drop(
+        ['Ticket', 'PassengerId', 'Name', 'Salutation', "Fare", 'Surname'],
+        axis=1,
+        inplace=True
+    )
+
+    X = train_data_df.drop(['Survived'], axis='columns')
+    y = train_data_df['Survived']
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=42
+    )
+
+    model1 = LogisticRegression()
+    model1.fit(X_train, y_train)
+    pred = model1.predict(X_test)
+    log_acc = accuracy_score(pred, y_test)
+    print(log_acc)
+    return model1
 
 
 def download_blob(bucket_name, source_blob_name, destination_file_name):
@@ -59,6 +89,14 @@ def download_blob(bucket_name, source_blob_name, destination_file_name):
     )
 
 
+def split_it(data):
+    result = re.search('^.*,(.*)\.\s.*$', data)
+    if result.group(1) not in [' Mr', ' Miss', ' Mrs', ' Master']:
+        return ' Misc'
+    else:
+        return result.group(1)
+
+
 if __name__ == "__main__":
     df = fetch_train_data()
-    train_model_from_data(df)
+    trained_model = train_model_from_data(df)
